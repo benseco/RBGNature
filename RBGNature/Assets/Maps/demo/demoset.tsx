215b80dda71ffd559eab6214d388efeb4ee08bec
benseco <ben.seco@live.com>
2019-03-31 20:42:28 -0500

    Demo map tmx

---
+++ RBGNature/Assets/Maps/demo/demoset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="demoset" tilewidth="20" tileheight="20" tilecount="170" columns="10">
+ <image source="demoset.png" width="200" height="341"/>
+</tileset>
